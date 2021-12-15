29d70618027723292b97330028fa689da61fde81
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-12-15 16:31:34 +0200

    Seperate Nav Link, Styled Badge, Nav User Avatar to a component

---
+++ src/Components/NavLink.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Nav } from 'react-bootstrap';
+
+export class NavLink extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        function changeBackground(e) {
+            e.target.style.transition = "all 0.5s"
+            if (e.type == "mouseover") {
+                e.target.style.transform = "scale(1.4)";
+                e.target.style.color = "gold";
+                return;
+            }
+            e.target.style.transform = "scale(1.0)";
+            e.target.style.color = "";
+            return;
+        }
+        const { title, route, extraClasses } = this.props;
+        return (
+            <Nav.Link className={"mx-2 " + extraClasses ? extraClasses : ''} href={route} onMouseLeave={changeBackground} onMouseOver={changeBackground}>{title}</Nav.Link>
+        )
+    }
+}
+
+export default NavLink
