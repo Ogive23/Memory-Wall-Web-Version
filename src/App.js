c90374b53aa8bbd486bd0c83b844982e7ac28f73
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-12-15 16:30:03 +0200

    Created Memory List Component

--- src/App.js
+++ src/App.js
@@ -2,55 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import './CSS/App.css';
 import CustomNavbar from './Components/CustomNavbar';
-import MemoryCard from './Components/MemoryCard';
-import { Col, Container, Row } from 'react-bootstrap';
-import { Fab, } from '@mui/material';
-import NavigationIcon from '@mui/icons-material/Navigation';
-import Box from '@mui/material/Box';
-//setup vars
-const memories = [
-  {
-    title: 'Title',
-    author: 'Author',
-    image: 'https://images-na.ssl-images-amazon.com/images/I/91DNhLLmUOL._AC_UL200_SR200,200_.jpg'
-  }, {
-    title: 'Title2',
-    author: 'Author2',
-    image: 'https://images-na.ssl-images-amazon.com/images/I/91DNhLLmUOL._AC_UL200_SR200,200_.jpg'
-  }
-];
-function MemoryList() {
-  return (
-    <Container>
-      <Row xs={1} md={3}>
-        {memories.map((memory) => { return <Col><MemoryCard memory={memory} /></Col> })}
-      </Row>
-      <Box sx={{ '& > :not(style)': { m: 1 } }}>
-        <Fab variant="extended" style={{ right: 20, position: 'fixed' }}>
-          <NavigationIcon sx={{ mr: 1 }} />
-          Navigate
-        </Fab>
-      </Box>
-    </Container>
-    // <section className="memorylist">
-
-    // </section>
-  );
-}
-// const Memory = (props) => {
-//   return <MemoryCard memory=
-//   return <article className="memory">
-//     <img src={props.memory.image} alt="" />
-//     <h1>{props.memory.title}</h1>
-//     <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}></h4>
-//   </article>
-// }
+import FloatingActionButton from './Components/FloatingActionButton';
+import MainMemoriesSlider from './Components/MainMemoriesSlider';
+import MemoryList from './Components/MemoryList';
 
 function App() {
-  return <main>
+  return <main id="main">
     <CustomNavbar />
-    {/* <MemoryList /> */}
-  </main>
+    <MainMemoriesSlider/>
+    <MemoryList />
+    <FloatingActionButton/>
+    </main>
 }
 
 export default App;
---
+++ src/Components/MemoryList.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import MemoryCard from './MemoryCard';
+const memories = [
+    {
+        id: '12671265',
+        personName: 'Omar Mohamed',
+        birthDate: '28/12/1983',
+        deathDate: '28/12/2000',
+        age: '17',
+        brief: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.',
+        lifeStory: '',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Pierre-Person.jpg',
+        createdOn: '17/11/2021 03:39:12',
+        numberOfLikes: 312,
+        numberOfShares: 14,
+        createdBy: {
+            id: '3456775643234567',
+            name: 'Mohamed Salah',
+            image: null,
+        }
+    }, {
+        id: '864231564321',
+        personName: 'Ali Mohamed',
+        birthDate: '16/11/1954',
+        deathDate: '09/10/2000',
+        age: '46',
+        brief: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.',
+        lifeStory: '',
+        image: 'https://www.theportlandclinic.com/wp-content/uploads/2019/07/Person-Curtis_4x5-e1564616444404-300x300.jpg',
+        createdOn: '11/02/2021 05:39:12', 
+        numberOfLikes: 123,
+        numberOfShares: 21,
+        createdBy: {
+            id: '13456775643234567',
+            name: 'Ali Salah',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9gqFKRn28xKHD1CAbEevdzsLmsv5yQkGnQ&usqp=CAU',
+        }
+    }
+];
+export class MemoryList extends Component {
+    render() {
+        return (
+            <Container>
+                <Row xs={1} md={3}>
+                    {memories.map((memory) => { return <Col><MemoryCard memory={memory} /></Col> })}
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default MemoryList
