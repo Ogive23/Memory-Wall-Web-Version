82544cefe2288c6b4e46275da7f18ca71be29bf3
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-12-15 16:32:20 +0200

    Transform Memory card to Material UI Component

--- src/Components/MemoryCard.js
+++ src/Components/MemoryCard.js
@@ -1,35 +1,76 @@
-import { Avatar } from '@mui/material';
 import React, { Component } from 'react'
-import { Card, Button, Row, Col } from 'react-bootstrap';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import { Badge, Button } from '@mui/material';
+import MemoryCardSettingsButton from './MemoryCardSettingsButton';
 
 export class MemoryCard extends Component {
     constructor(props) {
         super(props);
     }
+
     render() {
-        const { memory, children } = this.props;
+        const { memory } = this.props;
         return (
-            <Card style={{ width: '20rem' }}>
-                <Card.Img variant="top" src={memory.image} />
-                <Card.Body>
-                    <Card.Title>{memory.title}</Card.Title>
-                    <Card.Text>
-                        <Row>
-                            <Col xs={3}>
-                                <Avatar alt="Remy Sharp" src="https://mui.com/static/images/avatar/1.jpg" />
-                            </Col>
-                            <Col xs={9}>
-                                <Row className="font-weight-bold">{memory.author.toUpperCase()}</Row>
-                                <Row className="font-weight-100">SOFTWARE ENGINEER</Row>
-                                
-                            </Col>
-                        </Row>
-                    </Card.Text>
-                    <Card.Subtitle>{children}</Card.Subtitle>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
+            <Card sx={{ maxWidth: 345 }} style={{  }}>
+                <CardHeader
+                    avatar={
+                        <Avatar alt={memory.createdBy.name} src={memory.createdBy.image ?? '#'} />
+                    }
+                    action={
+                        <MemoryCardSettingsButton />
+                    }
+                    title={memory.createdBy.name}
+                    subheader={memory.createdOn}
+                />
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image={memory.image}
+                    alt={memory.personName}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {memory.personName}
+                    </Typography>
+                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                        {memory.birthDate + " - " + memory.deathDate}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {memory.brief}
+                    </Typography>
+                    <Button size="small">Know more about {memory.personName}</Button>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <Badge color="secondary" badgeContent={memory.numberOfLikes} max={999} showZero
+                        overlap="circular" anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}>
+                        <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                        </IconButton>
+                    </Badge>
+                    <Badge color="success" badgeContent={memory.numberOfShares} max={999} showZero
+                        overlap="circular" anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}>
+                        <IconButton aria-label="share">
+                            <ShareIcon />
+                        </IconButton>
+                    </Badge>
+                </CardActions>
             </Card>
-        )
+        );
     }
 }
 
