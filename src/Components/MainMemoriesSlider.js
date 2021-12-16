ba2f218bfe4a1ebe3a36b8ba40148b97eabe8682
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-12-16 12:47:15 +0200

    Solve Slider issue

--- src/Components/MainMemoriesSlider.js
+++ src/Components/MainMemoriesSlider.js
@@ -1,30 +1,49 @@
 import React, { Component } from 'react'
-import {Carousel , Item} from 'react-material-ui-carousel';
-const items = [
-    {
-        name: "Random Name #1",
-        description: "Probably the most random thing you have ever seen!"
-    },
-    {
-        name: "Random Name #2",
-        description: "Hello World!"
-    }
-]
+import { Carousel } from 'react-bootstrap'
+
 export class MainMemoriesSlider extends Component {
-    
+
     render() {
-        
         return (
-            <Carousel
-                NextIcon={<img src="http://random.com/next" />}
-                PrevIcon={<img src="http://random.com/prev" />}
-                    next={(next, active) => console.log(`we left ${active}, and are now at ${next}`)}
-                    prev={(prev, active) => console.log(`we left ${active}, and are now at ${prev}`)}
-            >
-                {
-                    items.map((item, i) => <Item key={i} item={item} />)
-                }
-            </Carousel>
+            <div>
+                <Carousel >
+                    <Carousel.Item >
+                        <img
+                            className="d-block w-100"
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyAGDzwgSydu29aao5rFL-18_jJ1GvMiv1GA&usqp=CAU"
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi-J1uJT75Eb0F_WecYmi77v12W3nAoB2GYQ&usqp=CAU"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         )
     }
 }
