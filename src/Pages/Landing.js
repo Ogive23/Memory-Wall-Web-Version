3896c327730ff6f15fc3d72ab8d48451dfa64f49
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2022-01-02 17:00:09 +0200

    Added Landing Page Background

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,9 +1,28 @@
 import { Box } from '@mui/material'
 import React, { useEffect, useState } from 'react'
+import { Background } from '../Components/Landing Page/Background'
+import { Footer } from '../Components/Landing Page/Footer'
+import { Records } from '../Components/Landing Page/Records'
 
 export const LandingPage = () => {
+    const [loadRecords, setLoadRecords] = useState(false);
+    const handleScroll = (e) => {
+        if (window.scrollY > 0) {
+            return setLoadRecords(true);
+        }
+        return setLoadRecords(false);
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
     return (
         <div>
+            <Background />
+            <Records loadRecords={loadRecords} />
+            {/* <Footer /> */}
         </div>
     )
 }
