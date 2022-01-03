88f2e6a1d735bc935d556abb8ea56662d8fd7011
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2022-01-03 16:59:11 +0200

    Update Memory List Component Structure

--- src/Components/Home Page/MemoryList.js
+++ src/Components/Home Page/MemoryList.js
@@ -1,13 +1,12 @@
-import { Box } from "@mui/material";
-import Pagination from "react-js-pagination";
+import { Pagination } from "@mui/material";
 import React, { useState, useEffect } from 'react';
 import { Col, Container, Row } from "react-bootstrap";
-// import { Factory } from "../Helpers/Factory";
 import MemoryCard from "./MemoryCard";
 import axios from 'axios';
+import { Factory } from "../../Helpers/Factory";
 import { useSelector } from "react-redux";
 
-function MemoryList() {
+export const MemoryList = () => {
   const [memories, setMemories] = useState([]);
   const [total, setTotal] = useState();
   const [current_page, setCurrrentPage] = useState(1);
@@ -19,11 +18,12 @@ function MemoryList() {
     console.log(`active page is ${pageNumber}`);
     fetchMemories(pageNumber);
   };
+
   function fetchMemories(current_page) {
-    axios.get(`http://127.0.0.1:8000/api/memorywall/memories?page=${current_page}`)
     axios.get(BASE_API_CALLER + `/api/memorywall/memories?page=${current_page}`)
       .then(res => {
-        setMemories(res.data.data.data);
+        let factory = new Factory();
+        setMemories(factory.getObjectsFromJson(res.data.data.data, "memories"));
         setPerPage(res.data.data.per_page);
         setTotal(res.data.data.total);
         setCurrrentPage(res.data.data.data.current_page);
@@ -37,67 +37,18 @@ function MemoryList() {
 
   if (error) return <h1>{error}</h1>;
   return (
-    <div>
-      <Container>
-        <Row xs={1} md={3} mb={4}>
-          {memories.map((memory) => {
-            return (
-              <Box pb={6}>
-                <Col key={memory.id}>
-                  <MemoryCard memory={memory} />
-                </Col>
-              </Box>
-
-            );
-          })}
-        </Row>
-
-        {/* <Pagination onPageChange={""} style={{ background: '#fff' }} count={10} /> */}
-        <Pagination
-          activePage={current_page}
-          itemsCountPerPage={per_page}
-          totalItemsCount={total}
-          pageRangeDisplayed={5}
-          onChange={(pageNumber) => { handlePageChange(pageNumber) }}
-          itemClass="page-item"
-          linkClass="page-link"
-          firstPageText='First'
-          lastPageText="Last"
-        />
-      </Container>
-    </div>
+    <Container>
+      <Row xs={1} md={3}>
+        {memories.map((memory) => {
+          return (
+            <Col key={memory.id}>
+              <MemoryCard memory={memory} />
+            </Col>
+          );
+        })}
+      </Row>
+      <Pagination onPageChange={""} style={{ background: '#fff' }} count={10} color="primary" />
+    </Container>
   )
 };
 export default MemoryList;
-// export class MemoryList extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       memories,
-//     };
-//   }
-//   render() {
-//     let factory = new Factory();
-//     try {
-//       this.state.memories = factory.getObjectsFromJson(memories, "memories");
-//       return (
-//         <Container>
-//           <Row xs={1} md={3}>
-//             {this.state.memories.map((memory) => {
-//               return (
-//                 <Col key={memory.id}>
-//                   <MemoryCard memory={memory} />
-//                 </Col>
-//               );
-//             })}
-//           </Row>
-//           <Pagination onPageChange={""} style={{ background: '#fff' }} count={10} color="primary" />
-//         </Container>
-//       );
-//     } catch (DataObjectTypeNotFound) {
-//       console.log(DataObjectTypeNotFound.message);
-//     }
-//   }
-// }
-
-// export default MemoryList;
